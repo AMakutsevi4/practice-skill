32b8980b6b221a658a099c96f7cfaf34aa302e47
Aleksandr Makutsevich <amakutsevich@bk.ru>
2025-03-28 19:29:45 +0300

    Completing 7 SQL INNER/LEFT JOIN tasks

--- src/main/java/ru/example/sql/join/cars.sql
+++ src/main/java/ru/example/sql/join/cars.sql
@@ -1,20 +1,26 @@
-CREATE TABLE cars (
-                      id int primary key,
-                      model text
+CREATE TABLE cars
+(
+    id    int primary key,
+    model text
 );
 
-CREATE TABLE engines (
-                         number int primary key,
-                         volume decimal,
-                         power int,
-                         car_id int references cars(id)
+CREATE TABLE engines
+(
+    number int primary key,
+    volume decimal,
+    power  int,
+    car_id int references cars (id)
 );
 
-INSERT INTO cars VALUES (1, 'Toyota Camry');
-INSERT INTO cars VALUES (2, 'Renault Sandero');
+INSERT INTO cars
+VALUES (1, 'Toyota Camry');
+INSERT INTO cars
+VALUES (2, 'Renault Sandero');
 
-INSERT INTO engines VALUES (1234, 2.5, 181, 1);
-INSERT INTO engines VALUES (5678, 1.2, 75, 2);
+INSERT INTO engines
+VALUES (1234, 2.5, 181, 1);
+INSERT INTO engines
+VALUES (5678, 1.2, 75, 2);
 
 
 /*Для того чтобы выполнить объединение между таблицами, используется внешний ключ (FOREIGN KEY) – это столбец
@@ -22,9 +28,13 @@ INSERT INTO engines VALUES (5678, 1.2, 75, 2);
 
 SELECT id, model, volume, power
 FROM engines e
-INNER JOIN cars c on e.car_id = c.id;
+         INNER JOIN cars c on e.car_id = c.id;
 
 
 SELECT c.id, c.model, e.volume, e.power
 FROM cars c
-INNER JOIN engines e ON e.car_id = c.id;
+         INNER JOIN engines e ON e.car_id = c.id;
+
+SELECT c.id, c.model, e.number, e.volume, e.power
+FROM cars c
+LEFT JOIN engines e ON e.car_id = c.id;
